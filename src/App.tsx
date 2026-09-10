05aae93d273bc8b06489ffe95a07d66975f7fbad
Aminul islam <aminulislam424842@gmail.com>
2026-09-10 19:04:36 +0600

    feat: add player data source

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import Navber from "./components/Navber"
 import Banner from "./components/Banner"
 import FooterSection from "./components/Footer";
 import { useState } from "react";
+import type { IPlayer } from "./types/player";
+
+async function playersFetch(): Promise<IPlayer[]> {
+  const res = await fetch('/data.json');
+  const data = await res.json();
+  return data;
+}
 
 function App() {
   const [coin, setCoin] = useState(15000);
